13e5c8c1fc8352f5f52e93b0cda8eceb2c2a1542
Sebastian HO <sebastianhodev@gmail.com>
2026-01-06 17:55:03 -0600

    dynamic routes

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from inspiring_quotes import get_random_quote
 
 
@@ -18,19 +18,32 @@ DAYS = [
 def index(request):
     return HttpResponse("Hello, world. You're at the quotes index.")
 
-def monday(request):
+# def monday(request):
+#     return HttpResponse("Hello, today is Monday.")
+
+# def tuesday(request):
+#     return HttpResponse("Hello, today is Tuesday.")
+
+def get_quote(day):
     quote = get_random_quote()
     text = ''
     
-    if quote:
+    if day.lower() == "tuesday":
+        text = "No quotes available today."
+        print(text)
+    elif quote:
         text = f"Quote: {quote['quote']}\nAuthor: {quote['author']}"
-        print(f"Quote: {quote['quote']}\nAuthor: {quote['author']}")
+        print(text)
     else:
-        text = "No quotes available."
-        print("No quotes available.")
-    return HttpResponse(f"Hello, today is Monday.\n {text}")
+        text = "No quotes found."
+        print(text)
+    
+    return text
 
-def tuesday(request):
-    return HttpResponse("Hello, today is Tuesday.")
-
+def days_week(request, day):
+    if day.lower() not in DAYS:
+        return HttpResponseNotFound("Not a valid day of the week.")
+    
+    quote_text = get_quote(day)    
+    return HttpResponse(f"Hello, today is {day.capitalize()}.\n {quote_text}")
 
