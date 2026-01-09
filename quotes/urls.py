6bc01104073cdf03d25150bfa355955eb55efd09
Sebastian HO <sebastianhodev@gmail.com>
2026-01-08 18:02:58 -0600

    django reverse function, index template v1

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("index", views.index),
+    path("", views.index),
     # path("monday", views.monday),
     # path("tuesday", views.tuesday),
     path("<int:day>", views.days_week_with_number, name="days_week_with_number"),
