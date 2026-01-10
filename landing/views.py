c023abd3379b0adf834312584eb020e7ade833a7
Sebastian HO <sebastianhodev@gmail.com>
2026-01-09 18:16:01 -0600

    templates filters

--- landing/views.py
+++ landing/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 # Create your views here.
 
 def home(request):
-    nombre = "Sebastian"
+    nombre = "sebastian"
+    today = date.today()
     
     return render(
         request, 
         "landing/landing.html", 
         {
             "name": nombre,
-            "age": 29
+            "age": 29,
+            "today": today
         }
     )
