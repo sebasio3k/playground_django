89ef5e89bd0f09b94cb6b1d25a955738d677094c
Sebastian HO <sebastianhodev@gmail.com>
2026-01-26 13:22:48 -0600

    model Genre, add genre manytomany to Book

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -8,12 +8,19 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
+class Genre(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     pages = models.IntegerField(null=True, blank=True)
     isbn = models.CharField(max_length=13, null=True, blank=True)
+    genre = models.ManyToManyField(Genre, related_name='books')
 
     def __str__(self):
         return self.title
