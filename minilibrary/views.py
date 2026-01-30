25d2abbd63b6c8ccb24cf93ed1d612f72801ba5e
Sebastian HO <sebastianhodev@gmail.com>
2026-01-30 16:30:53 -0600

    minilibrary index template, routes

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    try:
+        return render(request, "minilibrary/index.html", {
+            'text': "Minilibrary Page index"
+        })
+    except:
+        return render(request, "404.html", status=404)
