e68fd5353aecf3697f1612db2362b4470ec0b4f9
Sebastian HO <sebastianhodev@gmail.com>
2026-01-30 17:00:51 -0600

    minilibrary filters in views and templates

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,11 +1,35 @@
+import logging
 from django.shortcuts import render
+
+from minilibrary.models import Author, Book
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 def index(request):
     try:
+        books = Book.objects.all()
+        author_id = request.GET.get('author')
+        genre_id = request.GET.get('genre')
+        author = None
+        
+        if author_id:
+            books = books.filter(author_id=author_id)
+            author = Author.objects.get(id=author_id)
+            
+        if genre_id:
+            books = books.filter(genres__id=genre_id)
+        
         return render(request, "minilibrary/index.html", {
-            'text': "Minilibrary Page index"
+            'text': "Minilibrary Page index",
+            'author_id': author_id,
+            'books': books,
+            'author': author,
+            'genre_id': genre_id
         })
-    except:
+    except Exception as e:
+        logger.error(f'Error: {e}')
         return render(request, "404.html", status=404)
+    
+    
